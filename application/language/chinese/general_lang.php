a2fa666ea87f9b5926865f14567f0c7ed424543b
liming802232 <124545729+liming802232@users.noreply.github.com>
2023-02-09 14:24:34 +0800

    Add files via upload
    添加中文翻译文件夹

---
+++ application/language/chinese/general_lang.php
@@ -0,0 +1,256 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/*Browser Tab Menu*/
+$lang['tab_news']           = '新闻';
+$lang['tab_forum']          = '论坛';
+$lang['tab_store']          = '商店';
+$lang['tab_bugtracker']     = 'Bugtracker';
+$lang['tab_changelogs']     = '更新日志';
+$lang['tab_pvp_statistics'] = 'PvP统计';
+$lang['tab_login']          = '登录';
+$lang['tab_register']       = '注册';
+$lang['tab_home']           = '主页';
+$lang['tab_donate']         = '捐赠';
+$lang['tab_vote']           = '投票';
+$lang['tab_cart']           = '信用卡';
+$lang['tab_account']        = '我的账户';
+$lang['tab_reset']          = '密码重置';
+$lang['tab_pending']        = '账户激活';
+$lang['tab_error']          = 'Error 404';
+$lang['tab_maintenance']    = '维护';
+$lang['tab_online']         = '在线玩家';
+$lang['tab_download']       = '下载';
+
+/*Panel Navbar*/
+$lang['navbar_vote_panel']   = '投票面板';
+$lang['navbar_donate_panel'] = '捐赠面板';
+
+/*Button Lang*/
+$lang['button_register']          = '注册';
+$lang['button_login']             = '登录';
+$lang['button_logout']            = '退出';
+$lang['button_activate']          = '激活';
+$lang['button_request']           = '请求新代码';
+$lang['button_forgot_password']   = '遗忘你的密码?';
+$lang['button_user_panel']        = '用户面板';
+$lang['button_admin_panel']       = '管理面板';
+$lang['button_mod_panel']         = '模块面板';
+$lang['button_change_avatar']     = '更换头像';
+$lang['button_add_personal_info'] = '添加个人信息';
+$lang['button_create_report']     = '创建报告';
+$lang['button_new_topic']         = '新话题';
+$lang['button_edit_topic']        = '编辑话题';
+$lang['button_save_changes']      = '保存变更';
+$lang['button_cancel']            = '取消';
+$lang['button_send']              = '发送';
+$lang['button_read_more']         = '继续阅读';
+$lang['button_add_reply']         = '加入回应';
+$lang['button_remove']            = '删除';
+$lang['button_create']            = '创建';
+$lang['button_save']              = '保存';
+$lang['button_close']             = '关闭';
+$lang['button_reply']             = '回复';
+$lang['button_donate']            = '捐赠';
+$lang['button_account_settings']  = '账户设置';
+$lang['button_cart']              = '增加信用卡';
+$lang['button_view_cart']         = '查看信用卡';
+$lang['button_checkout']          = '结账';
+$lang['button_buying']            = '继续购买';
+
+/*Alert Lang*/
+$lang['alert_successful_purchase'] = '物品购买成功';
+$lang['alert_upload_error']        = '你图片的必须是JPG或png格式';
+$lang['alert_changelog_not_found'] = '服务器此时没有要通知的更改日志';
+$lang['alert_points_insufficient'] = '积分不足';
+
+/*Status Lang*/
+$lang['offline'] = '离线';
+$lang['online']  = '在线';
+
+/*Label Lang*/
+$lang['label_open']   = '打开';
+$lang['label_closed'] = '关闭';
+
+/*Form Label Lang*/
+$lang['label_login_info'] = '登入信息';
+
+/*Input Placeholder Lang*/
+$lang['placeholder_username']          = '用户名';
+$lang['placeholder_email']             = '电子邮件地址';
+$lang['placeholder_password']          = '密码';
+$lang['placeholder_re_password']       = '重复密码';
+$lang['placeholder_current_password']  = '当前密码';
+$lang['placeholder_new_password']      = '新密码';
+$lang['placeholder_new_username']      = '新用户名';
+$lang['placeholder_confirm_username']  = '确认新用户名';
+$lang['placeholder_new_email']         = '新电子邮件';
+$lang['placeholder_confirm_email']     = '确认新电子邮件';
+$lang['placeholder_create_bug_report'] = '创建bug报告';
+$lang['placeholder_title']             = '标题';
+$lang['placeholder_type']              = '类型';
+$lang['placeholder_description']       = '说明';
+$lang['placeholder_url']               = '网址';
+$lang['placeholder_uri']               = '友好网址 (例如: tos)';
+$lang['placeholder_highl']             = 'Highlight';
+$lang['placeholder_lock']              = '锁定';
+$lang['placeholder_subject']           = '主题';
+
+/*Table header Lang*/
+$lang['table_header_name']             = '名称';
+$lang['table_header_faction']          = '阵营';
+$lang['table_header_total_kills']      = '总计击杀';
+$lang['table_header_today_kills']      = '今日击杀';
+$lang['table_header_yersterday_kills'] = '昨日击杀';
+$lang['table_header_team_name']        = '团队名称';
+$lang['table_header_members']          = '成员';
+$lang['table_header_rating']           = '评级';
+$lang['table_header_games']            = '游戏';
+$lang['table_header_id']               = 'ID';
+$lang['table_header_status']           = '状态';
+$lang['table_header_priority']         = '重点';
+$lang['table_header_date']             = '日期';
+$lang['table_header_author']           = '创作者';
+$lang['table_header_time']             = '时间';
+$lang['table_header_icon']             = '图标';
+$lang['table_header_realm']            = 'Realm';
+$lang['table_header_zone']             = '区域';
+$lang['table_header_character']        = '角色';
+$lang['table_header_price']            = '价格';
+$lang['table_header_item_name']        = '物品名称';
+$lang['table_header_items']            = '物品(s)';
+$lang['table_header_quantity']         = '数量';
+
+/*Class Lang*/
+$lang['class_warrior']     = '战士';
+$lang['class_paladin']     = '圣骑士';
+$lang['class_hunter']      = '猎人';
+$lang['class_rogue']       = '盗贼';
+$lang['class_priest']      = '牧师';
+$lang['class_dk']          = '死亡骑士';
+$lang['class_shaman']      = '萨满';
+$lang['class_mage']        = '法师';
+$lang['class_warlock']     = '术士';
+$lang['class_monk']        = '武僧';
+$lang['class_druid']       = '德鲁伊';
+$lang['class_demonhunter'] = '恶魔猎人';
+
+/*Faction Lang*/
+$lang['faction_alliance'] = '联盟';
+$lang['faction_horde']    = '部落';
+
+/*Gender Lang*/
+$lang['gender_male']   = '男性';
+$lang['gender_female'] = '女性';
+
+/*Race Lang*/
+$lang['race_human']               = '人类';
+$lang['race_orc']                 = '兽人';
+$lang['race_dwarf']               = '矮人';
+$lang['race_night_elf']           = '暗夜精灵';
+$lang['race_undead']              = '亡灵';
+$lang['race_tauren']              = '牛头人';
+$lang['race_gnome']               = '侏儒';
+$lang['race_troll']               = '巨魔';
+$lang['race_goblin']              = '地精';
+$lang['race_blood_elf']           = '血精灵';
+$lang['race_draenei']             = '德莱尼';
+$lang['race_worgen']              = '狼人';
+$lang['race_panda_neutral']       = '中立熊猫人';
+$lang['race_panda_alli']          = '联盟熊猫人';
+$lang['race_panda_horde']         = '部落熊猫人';
+$lang['race_nightborde']          = '夜之子';
+$lang['race_void_elf']            = '虚空精灵';
+$lang['race_lightforged_draenei'] = '光铸德莱尼';
+$lang['race_highmountain_tauren'] = '至高岭牛头人';
+$lang['race_dark_iron_dwarf']     = '黑铁矮人';
+$lang['race_maghar_orc']          = '德拉诺兽人';
+
+/*Header Lang*/
+$lang['header_cookie_message'] = '本网站使用cookies来确保您获得我们网站的最佳体验';
+$lang['header_cookie_button']  = '找到了!';
+
+/*Footer Lang*/
+$lang['footer_rights'] = '版权所有';
+
+/*Page 404 Lang*/
+$lang['page_404_title']       = '404页面未找到';
+$lang['page_404_description'] = ''您访问的页面不存在';
+
+/*Panel Lang*/
+$lang['panel_acc_rank']           = '账户等级';
+$lang['panel_dp']                 = '捐赠积分';
+$lang['panel_vp']                 = '投票积分';
+$lang['panel_expansion']          = '扩展';
+$lang['panel_member']             = '成员来自';
+$lang['panel_chars_list']         = '角色列表';
+$lang['panel_account_details']    = '账户明细';
+$lang['panel_last_ip']            = '最后 IP';
+$lang['panel_request_ip']         = '请求的IP';
+$lang['panel_change_email']       = '更改电子邮件地址';
+$lang['panel_change_username']    = '更改用户名';
+$lang['panel_change_password']    = '更改密码';
+$lang['panel_replace_pass_by']    = '密码替换为';
+$lang['panel_current_username']   = '当前用户名';
+$lang['panel_current_email']      = '当前电子邮件地址';
+$lang['panel_replace_email_by']   = '电子邮件替换为';
+$lang['panel_server_checkstatus'] = '服务器处于脱机状态,无法购买.';
+
+/*Home Lang*/
+$lang['home_latest_news']   = '最新新闻';
+$lang['home_discord']       = '不和谐';
+$lang['home_server_status'] = '服务器状态';
+$lang['home_set_realmlist'] = '下载领域列表';
+$lang['home_realm_info']    = '当前列表是';
+
+/*PvP Statistics Lang*/
+$lang['statistics_top_20']  = '前20排名';
+$lang['statistics_top_2v2'] = '前2V2排名';
+$lang['statistics_top_3v3'] = '前3V3排名';
+$lang['statistics_top_5v5'] = '前5V5排名';
+
+/*News Lang*/
+$lang['news_recent_list'] = '最近新闻列表';
+$lang['news_comments']    = '评论';
+
+/*Bugtracker Lang*/
+$lang['bugtracker_report_notfound'] = '报告未找到';
+
+/*Donate Lang*/
+$lang['donate_get'] = '获取';
+
+/*Vote Lang*/
+$lang['vote_next_time'] = '下一次投票:';
+
+/*Forum Lang*/
+$lang['forum_posts_count']        = 'Posts';
+$lang['forum_topic_locked']       = '此主题已锁定';
+$lang['forum_comment_locked']     = '有话要说吗？登录以加入主题对话';
+$lang['forum_comment_locked_act'] = '有什么要说的吗？请先激活您的帐户';
+$lang['forum_comment_header']     = '加入对话';
+$lang['forum_not_authorized']     = '未授权';
+$lang['forum_post_history']       = '查看帖子历史记录';
+$lang['forum_topic_list']         = '主题列表';
+$lang['forum_last_activity']      = '最新活动';
+$lang['forum_last_post_by']       = '最后发布人';
+$lang['forum_whos_online']        = '谁在线';
+$lang['forum_replies_count']      = 'Replies';
+$lang['forum_topics_count']       = '主题';
+$lang['forum_users_count']        = '用户';
+
+/*Store Lang*/
+$lang['store_categories']    = '商店类别';
+$lang['store_top_items']     = '排行榜';
+$lang['store_cart_added']    = '您已添加';
+$lang['store_cart_in_your']  = '在您的购物车中';
+$lang['store_cart_no_items'] = '您的购物车中没有商品';
+
+/*Soap Lang*/
+$lang['soap_send_subject'] = '商店购买';
+$lang['soap_send_body']    = '感谢您在本店购买!';
+
+/*Email Lang*/
+$lang['email_password_recovery_p1'] = '密码恢复确认';
+$lang['email_password_recovery_p2'] = '密码恢复';
+$lang['email_account_activation']   = '激活您的 %s 账户';
---
+++ application/language/chinese/rest_controller_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['text_rest_invalid_api_key']       = '无效的API秘钥%s'; // %s是REST API秘钥
+$lang['text_rest_invalid_credentials']   = 'e无效凭证';
+$lang['text_rest_ip_denied']             = 'IP被拒绝';
+$lang['text_rest_ip_unauthorized']       = 'IP未经授权';
+$lang['text_rest_unauthorized']          = '未授权';
+$lang['text_rest_ajax_only']             = '仅允许AJAX请求';
+$lang['text_rest_api_key_unauthorized']  = '此API密钥无权访问请求的控制器';
+$lang['text_rest_api_key_permissions']   = '此API密钥没有足够的权限';
+$lang['text_rest_api_key_time_limit']    = '此API密钥已达到此方法的时间限制';
+$lang['text_rest_ip_address_time_limit'] = '此IP地址已达到此方法的时间限制';
+$lang['text_rest_unknown_method']        = '未知方法';
+$lang['text_rest_unsupported']           = '不支持的协议';
