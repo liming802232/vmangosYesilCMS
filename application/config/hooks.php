d872d07e6caed770582266dfac99ec0b1fd2b8ab
liming802232 <124545729+liming802232@users.noreply.github.com>
2023-02-10 10:33:14 +0800

    Update hooks.php
    add zh

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -4,10 +4,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
-| Hooks
+| Hooks 钩子
 | -------------------------------------------------------------------------
-| This file lets you define "hooks" to extend CI without hacking the core
-| files.  Please see the user guide for info:
+| 该文件允许您定义Hooks“钩子”以扩展 CI 而无需破解核心
+| 文件。 有关信息，请参阅用户指南：
 |
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
