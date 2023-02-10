15e6a09dce305d5f249fabb9a6c18f81e59303c9
liming802232 <124545729+liming802232@users.noreply.github.com>
2023-02-10 18:46:21 +0800

    Update routes.php
    addzh

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,50 +4,50 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
+| URI ROUTING   URI路由
+| ---------------------------------------------- ----------------------
+| 该文件允许您将 URI 请求重新映射到特定的控制器功能。
 |
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
+| 通常，URL 字符串之间存在一对一的关系
+| 及其相应的控制器类/方法。
+| URL 中的段通常遵循以下模式：
 |
-|	example.com/class/method/id/
+| example.com/class/method/id/
 |
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
+| 然而，在某些情况下，您可能希望重新映射这种关系
+| 以便调用一个不同的类/函数
+| 与网址相对应。
 |
-| Please see the user guide for complete details:
+| 请参阅用户指南以获取完整的详细信息：
 |
 |	https://codeigniter.com/user_guide/general/routing.html
 |
 | -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
+| RESERVED ROUTES   保留路径
+| ---------------------------------------------- ----------------------
 |
-| There are three reserved routes:
+| 有3条预留路线：
 |
-|	$route['default_controller'] = 'welcome';
+| $route['default_controller'] = 'welcome';
 |
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
+| 此路由指示应加载哪个控制器类，如果
+| URI 不包含任何数据。 在上面的例子中，"welcome" 类
+| 会被加载。
 |
-|	$route['404_override'] = 'errors/page_missing';
+| $route['404_override'] = 'errors/page_missing';
 |
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
+| 这条路线将告诉路由器使用哪个控制器/方法，如果这些
+| URL 中提供的无法与有效路由匹配。
 |
-|	$route['translate_uri_dashes'] = FALSE;
+| $route['translate_uri_dashes'] = FALSE;
 |
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
+| 这不完全是一条路线，但允许您自动路线
+| 包含破折号的控制器和方法名称。 '-' 无效
+| 类或方法名字符，所以需要翻译。
+| 当您将此选项设置为 TRUE 时，它将替换
+| 控制器和方法 URI 段。
 |
-| Examples:	my-controller/index	-> my_controller/index
+| 示例:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
 $lang = '^(en|es|bl|fr|de|ru)';
@@ -57,8 +57,8 @@ $route['404_override']         = 'general/error404';
 $route['translate_uri_dashes'] = false;
 
 /*
- *  Website Routes
- *  Configuration paths used by the CMS
+ *  Website Routes 网站路由
+ *  Configuration paths used by the CMS CMS使用的配置路径
 */
 $route[$lang . '$']            = $route['default_controller'];
 $route[$lang . '/confmigrate'] = 'home/setconfig';
@@ -67,8 +67,8 @@ $route[$lang . '/maintenance'] = 'general/maintenance';
 $route[$lang . '/realmlist']   = 'home/downloadRealmlist';
 
 /*
- *  User
- *  Routes with functionalities for the user.
+ *  User 用户
+ *  Routes with functionalities for the user. 为用户提供功能的路径。
 */
 $route[$lang . '/login']                            = 'user/login';
 $route[$lang . '/register']                         = 'user/register';
@@ -90,23 +90,23 @@ $route[$lang . '/changeavatar']                     = 'user/newavatar';
 $route[$lang . '/changeusername']                   = 'user/newusername';
 
 /*
- *  Vote
- *  Voting system guidelines
+ *  Vote 投票
+ *  Voting system guidelines* 投票系统指南
 */
 $route[$lang . '/vote']                = 'vote/index';
 $route[$lang . '/vote/votenow/(:num)'] = 'vote/votenow/$2';
 
 /*
- *  Donate
- *  Donations module, routes for the user
+ *  Donate 捐赠
+ *  Donations module, routes for the user *捐赠模块，用户路线
 */
 $route[$lang . '/donate']              = 'donate/index';
 $route[$lang . '/donate/check/(:any)'] = 'donate/check/$2';
 $route[$lang . '/donate/canceled']     = 'donate/canceled';
 
 /*
- *  Download
- *  Download module, where addons and clients can be added.
+ *  Download 下载
+ *  Download module, where addons and clients can be added.* 下载模块，可以在其中添加插件和客户端。
 */
 $route[$lang . '/download']                   = 'download/index';
 $route[$lang . '/admin/download']             = 'admin/managedownload';
@@ -117,14 +117,14 @@ $route[$lang . '/admin/download/update']      = 'admin/updatedownload';
 $route[$lang . '/admin/download/delete']      = 'admin/deletedownload';
 
 /*
- *  Changelog
- *  Route for changes in the project
+ *  Changelog 变更日志
+ *  Route for changes in the project* 项目变更路线
 */
 $route[$lang . '/changelogs'] = 'changelogs/index';
 
 /*
- *  Bugtracker
- *  Error reporting system
+ *  Bugtracker 错误追踪器
+ *  Error reporting system* 错误反馈系统
 */
 $route[$lang . '/bugtracker']               = 'bugtracker/index';
 $route[$lang . '/bugtracker/(:num)']        = 'bugtracker/index/$2';
@@ -133,8 +133,8 @@ $route[$lang . '/bugtracker/create']        = 'bugtracker/create';
 $route[$lang . '/bugtracker/report/(:num)'] = 'bugtracker/report/$2';
 
 /*
- *  Forum
- *  Own forum within the CMS, still under development.
+ *  Forum 论坛
+ *  Own forum within the CMS, still under development.* CMS 内置的论坛，仍在开发中。
 */
 $route[$lang . '/forum']                    = 'forum/index';
 $route[$lang . '/forum/category/(:num)']    = 'forum/category/$2';
@@ -146,16 +146,16 @@ $route[$lang . '/forum/topic/reply/delete'] = 'forum/deletereply';
 $route[$lang . '/forum/topic/update']       = 'forum/update';
 
 /*
- *  News
- *  News module, to visualize and make comments.
+ *  News 新闻
+ *  News module, to visualize and make comments.* 新闻模块，可视化和发表评论。
 */
 $route[$lang . '/news/(:num)']       = 'news/article/$2';
 $route[$lang . '/news/reply']        = 'news/reply';
 $route[$lang . '/news/reply/delete'] = 'news/deletereply';
 
 /*
- *  Store
- *  Routes over the store
+ *  Store 商店
+ *  Routes over the store* 商店内的路线
 */
 $route[$lang . '/store']                = 'store/index';
 $route[$lang . '/store/(:any)']         = 'store/category/$2/';
@@ -167,27 +167,27 @@ $route[$lang . '/cart/updatequantity']  = 'store/updatequantity';
 $route[$lang . '/cart/updatecharacter'] = 'store/updatecharacter';
 
 /*
- *  Pages
- *  Static pages, which can be created from the admin panel.
+ *  Pages 页面
+ *  Static pages, which can be created from the admin panel.* 静态页面，可以从管理面板创建。
 */
 $route[$lang . '/page/(:any)'] = 'page/index/$2/';
 
 /*
- *  PVP
- *  PVP statistics of the server and its realms.
- *  It also includes arena statistics.
+ *  PVP* 玩家对战
+ *  PVP statistics of the server and its realms.* 服务器及其领域的 PVP 统计数据。
+ *  It also includes arena statistics.* 它还包括竞技场统计数据。
 */
 $route[$lang . '/pvp'] = 'pvp/index';
 
 /*
- *  Online
- *  Information about people active within the server.
+ *  Online 在线
+ *  Information about people active within the server.* 有关服务器内活跃人员的信息。
 */
 $route[$lang . '/online'] = 'online/index';
 
 /*
- *  Armory
- *  Information about player, items or guild.
+ *  Armory 军械库
+ *  Information about player, items or guild.* 关于玩家、物品或公会的信息。
 */
 $route[$lang . '/armory']                      = 'armory';
 $route[$lang . '/armory/search']               = 'armory/search';
@@ -197,15 +197,15 @@ $route[$lang . '/armory/guild/(:num)/(:num)']  = 'armory/guild/$2/$3';
 
 /*
  *  Api
- *  Initial API structure to develop CMS further
+ *  Initial API structure to develop CMS further* 进一步开发 CMS 的初始 API 结构
 */
 $route[$lang . '/api/v1']                          = 'api_v1';
 $route[$lang . '/api/v1/item/newdisplayid/(:num)'] = 'api_v1/classic_displayid/$2';
 
 
 /*
- *  Mod Routes
- *  Route for moderators
+ *  Mod Routes 版主路由
+ *  Route for moderators 版主路径
 */
 $route[$lang . '/mod']          = 'mod/index';
 $route[$lang . '/mod/queue']    = 'mod/queue';
@@ -215,8 +215,8 @@ $route[$lang . '/mod/bannings'] = 'mod/bannings';
 $route[$lang . '/mod/warnings'] = 'mod/warnings';
 
 /*
- *  Admin Routes
- *  Routes for administrators
+ *  Admin Routes 管理路由
+ *  Routes for administrators  管理员路由
 */
 $route[$lang . '/admin']                              = 'admin/index';
 $route[$lang . '/admin/cms']                          = 'admin/cmsmanage';
@@ -235,7 +235,7 @@ $route[$lang . '/admin/modules/enable']               = 'admin/enablemodule';
 $route[$lang . '/admin/modules/disable']              = 'admin/disablemodule';
 
 /*
- *  Manage Accounts
+ *  Manage Accounts 管理账户
 */
 $route[$lang . '/admin/accounts']              = 'admin/accounts';
 $route[$lang . '/admin/accounts/(:num)']       = 'admin/accounts/$2';
@@ -248,7 +248,7 @@ $route[$lang . '/admin/account/grantrank']     = 'admin/grantrankaccount';
 $route[$lang . '/admin/account/delrank']       = 'admin/delrankaccount';
 
 /*
- *	Tickets
+ *	Tickets 管理门票
 */
 $route[$lang . '/admin/tickets']              = 'admin/managetickets';
 $route[$lang . '/admin/tickets/realm/(:num)'] = 'admin/ticketrealm/$2';
@@ -256,7 +256,7 @@ $route[$lang . '/admin/tickets/realm/(:num)/(:num)']
                                               = 'admin/ticketrealm/$2/$3';
 
 /*
- *  Menu
+ *  Menu 管理菜单
 */
 $route[$lang . '/admin/menu']             = 'admin/managemenu';
 $route[$lang . '/admin/menu/create']      = 'admin/createmenu';
@@ -266,7 +266,7 @@ $route[$lang . '/admin/menu/update']      = 'admin/updatemenu';
 $route[$lang . '/admin/menu/delete']      = 'admin/deletemenu';
 
 /*
- *  Realms
+ *  Realms 管理领域
 */
 $route[$lang . '/admin/realms']             = 'admin/managerealms';
 $route[$lang . '/admin/realms/(:num)']      = 'admin/managerealms/$2';
@@ -277,7 +277,7 @@ $route[$lang . '/admin/realms/update']      = 'admin/updaterealm';
 $route[$lang . '/admin/realms/delete']      = 'admin/deleterealm';
 
 /*
- *  Slides
+ *  Slides 管理幻灯片
 */
 $route[$lang . '/admin/slides']             = 'admin/manageslides';
 $route[$lang . '/admin/slides/(:num)']      = 'admin/manageslides/$2';
@@ -288,7 +288,7 @@ $route[$lang . '/admin/slides/update']      = 'admin/updateslide';
 $route[$lang . '/admin/slides/delete']      = 'admin/deleteslide';
 
 /*
- *  News
+ *  News 管理新闻
 */
 $route[$lang . '/admin/news']             = 'admin/managenews';
 $route[$lang . '/admin/news/(:num)']      = 'admin/managenews/$2';
@@ -297,7 +297,7 @@ $route[$lang . '/admin/news/edit/(:num)'] = 'admin/editnews/$2';
 $route[$lang . '/admin/news/delete']      = 'admin/deletenews';
 
 /*
- *  Changelog
+ *  Changelog 管理变更日志
 */
 $route[$lang . '/admin/changelogs']             = 'admin/managechangelogs';
 $route[$lang . '/admin/changelogs/(:num)']      = 'admin/managechangelogs/$2';
@@ -308,7 +308,7 @@ $route[$lang . '/admin/changelogs/update']      = 'admin/updatechangelog';
 $route[$lang . '/admin/changelogs/delete']      = 'admin/deletechangelog';
 
 /*
- *  Pages
+ *  Pages 管理页面
 */
 $route[$lang . '/admin/pages']             = 'admin/managepages';
 $route[$lang . '/admin/pages/(:num)']      = 'admin/managepages/$2';
@@ -319,7 +319,7 @@ $route[$lang . '/admin/pages/update']      = 'admin/updatepage';
 $route[$lang . '/admin/pages/delete']      = 'admin/deletepage';
 
 /*
- *  Store
+ *  Store 管理商店
 */
 $route[$lang . '/admin/store']                      = 'admin/managestore';
 $route[$lang . '/admin/store/(:num)']               = 'admin/managestore/$2';
@@ -345,7 +345,7 @@ $route[$lang . '/admin/store/top/delete']           = 'admin/deletestoretop';
 $route[$lang . '/admin/store/logs']                 = 'admin/storelogs';
 
 /*
- *  Donate
+ *  Donate 管理捐赠
 */
 $route[$lang . '/admin/donate']             = 'admin/donate';
 $route[$lang . '/admin/donate/create']      = 'admin/createdonateplan';
@@ -356,7 +356,7 @@ $route[$lang . '/admin/donate/delete']      = 'admin/deletedonateplan';
 $route[$lang . '/admin/donate/logs']        = 'admin/donatelogs';
 
 /*
- *  Topsites
+ *  Topsites 管理热门
 */
 $route[$lang . '/admin/topsites']             = 'admin/managetopsites';
 $route[$lang . '/admin/topsites/(:num)']      = 'admin/managetopsites/$2';
@@ -367,7 +367,7 @@ $route[$lang . '/admin/topsites/update']      = 'admin/updatetopsite';
 $route[$lang . '/admin/topsites/delete']      = 'admin/deletetopsite';
 
 /*
- *  Forum
+ *  Forum 管理论坛
 */
 $route[$lang . '/admin/forum']                      = 'admin/manageforum';
 $route[$lang . '/admin/forum/(:num)']               = 'admin/manageforum/$2';
@@ -385,11 +385,11 @@ $route[$lang . '/admin/forum/category/update']      = 'admin/updateforumcategory
 $route[$lang . '/admin/forum/category/delete']      = 'admin/deleteforumcategory';
 
 /*
- *  Vote (admin)
+ *  Vote (admin)   投票（管理员）
 */
 $route[$lang . '/admin/vote/logs'] = 'admin/votelogs';
 
 /*
- *  To check the soap connection
+ *  To check the soap connection   检查 soap 连接
 */
 $route[$lang . '/admin/checksoap'] = 'admin/checkSoap';
